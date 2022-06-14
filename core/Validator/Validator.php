4b9f1694496e59e0eafd6225f7ea8be42150b84e
=MrMsK <=>
2022-06-14 13:08:21 +0200

    add rules

--- core/Validator/Validator.php
+++ core/Validator/Validator.php
@@ -7,25 +7,30 @@ namespace Core\Validator;
 class validator
 {
 
+    /**
+     * @var array
+     */
     public $validators = [];
 
     /**
-     * @param $inputs
-     * @param $rules
+     * @param array $inputs
+     * @param array $rules
      * @return Validation
      */
-    public function make($inputs, $rules){
+    public function make(array $inputs, array $rules){
         $this->registerRules();
         $validation = new Validation($this, $inputs, $rules);
         return $validation;
     }
+
 
     protected  function registerRules(){
         $baseRules = [
             'Email' => \Core\Validator\Types\Email::class,
             'Text' => \Core\Validator\Types\Text::class,
             'Required' => \Core\Validator\Types\Required::class,
-
+            'Lenght' => \Core\Validator\Types\Lenght::class,
+            'Interger' => \Core\Validator\Types\Interger::class,
         ];
         foreach ($baseRules as $key => $rule){
             $this->validators[$key] = $rule;

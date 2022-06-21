849604148ff3eaa4ed3d869a70f86b39137e356c
= <=>
2022-06-21 13:03:43 +0200

    add template 404

--- public/index.php
+++ public/index.php
@@ -18,20 +18,30 @@ if(isset($_GET['action'])){
 $action = explode('.', $action);
 
 
+
+
 if($action[0] == 'admins' || $action[0] == 'users'){
     if(empty($action[2])){
       App::getInstance()->getControllers()->notFound();
     }
     $controller = '\App\Controllers\\'.$action[0].'\\'. ucfirst($action[1]).'Controller';
-
+if(method_exists($controller, $action[2])){
     $method = $action[2];
+}else{
+    App::getInstance()->getControllers()->notFound();
+}
 
 }else{
-    if(empty($action[1])){
+    $dir =scandir(ROOT.DIRECTORY_SEPARATOR.'app/Controllers');
+    if(!in_array(ucfirst($action[0]).'Controller.php', $dir)){
         App::getInstance()->getControllers()->notFound();
     }
-        $controller = '\App\Controllers\\'.ucfirst($action[0]).'Controller';
+    $controller = '\App\Controllers\\'.ucfirst($action[0]).'Controller';
+    if(method_exists($controller, $action[1])){
         $method = $action[1];
+    }else{
+        App::getInstance()->getControllers()->notFound();
+    }
 
 }
 
